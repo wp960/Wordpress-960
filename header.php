7d4daa24bebd1b485598f580f3257e8451da12ad
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-01-17 14:56:08 -0800

    Adding the 960 Grid System. Adding the header and the footer.

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title></title>
+    <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
+  </head>
+  <body>
--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<?php
-  # I can haz post?
-  if ( have_posts() ) :
-    while ( have_posts() ) :
-      the_post();
-      the_content();
-    endwhile;
-  else:
-    # No posts.
-  endif;
-?>
+<?php get_header(); ?>
+
+<div class="container_12">
+  <?php
+    # I can haz post?
+    if ( have_posts() ) :
+      while ( have_posts() ) :
+        the_post();
+        echo '<div class="grid_12">';
+        the_content();
+        echo '</div>';
+      endwhile;
+    else:
+      # No posts.
+    endif;
+  ?>
+</div>
+
+<?php get_footer(); ?>
