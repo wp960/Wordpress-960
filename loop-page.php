78f3f9cfe60d74bef66ef2fad8c4c79067e38235
Michael Enger <mike@thelonelycoder.com>
2012-03-30 13:02:29 +0200

    Fix missing translation namespaces

--- loop-page.php
+++ loop-page.php
@@ -5,7 +5,7 @@
 	<article class="post">
 
 		<h2 class="title">
-			<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php echo _e('Permanent Link to '); the_title_attribute(); ?>"><?php the_title(); ?></a>
+			<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php echo _e( 'Permanent Link to ', 'wp960' ); the_title_attribute(); ?>"><?php the_title(); ?></a>
 		</h2>
 
 		<div class="entry">
