62993f5933919d6b1abcbd44a473f3b6032a8f7d
Michael Enger <mike@thelonelycoder.com>
2012-03-30 09:58:08 +0200

    Footer widgets

--- footer.php
+++ footer.php
@@ -1,9 +1,19 @@
-		<!-- Header Navigation -->
-		<?php wp_nav_menu( array(
-			'theme_location' => 'footer',
-			'menu_class'    => 'nav-bar',
-			'container'     => 'nav'
-		) ); ?>
+		<footer class="grid_12">
+
+			<!-- Footer Widgets -->
+			<aside>
+				<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?><?php endif; ?>
+			</aside>
+
+			<!-- Footer Navigation -->
+			<?php wp_nav_menu( array(
+				'theme_location' => 'footer',
+				'menu_class'    => 'nav-bar',
+				'container'     => 'nav'
+			) ); ?>
+
+		</footer>
+
 		</div>
 		<?php wp_footer(); ?>
 	</body>
